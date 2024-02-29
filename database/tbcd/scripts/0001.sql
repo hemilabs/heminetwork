343f902f5513be6213fae37fd52fe36693a191f8
Marco Peereboom <marco@peereboom.us>
2024-02-29 14:03:47 +0000

    Switch to block header downloads for ibd

--- database/tbcd/scripts/0001.sql
+++ database/tbcd/scripts/0001.sql
@@ -7,14 +7,16 @@ CREATE TABLE version (version INTEGER UNIQUE NOT NULL);
 INSERT INTO version (version) VALUES (1);
 
 -- btc hash height maps blocks to their height to potentially multiple hashes
-CREATE TABLE btc_hash_height (
+CREATE TABLE block_headers (
 	hash		BYTEA PRIMARY KEY UNIQUE NOT NULL,
 	height		BIGINT NOT NULL,
+	header		BYTEA UNIQUE NOT NULL,
 	created_at	TIMESTAMP NOT NULL DEFAULT NOW(),
 
-	CONSTRAINT btc_hash_height_contraint UNIQUE (hash, height),
-	CONSTRAINT btc_hash_height_hash_length CHECK (octet_length(hash) = 32)
+	CONSTRAINT block_headers_hash_height UNIQUE (hash, height),
+	CONSTRAINT block_headers_hash_length CHECK (octet_length(hash) = 32),
+	CONSTRAINT block_headers_header_length CHECK (octet_length(header) = 80)
 );
-CREATE INDEX btc_hash_height_index  ON btc_hash_height (height);
+CREATE INDEX block_headers_height_index  ON block_headers (height);
 
 COMMIT;
