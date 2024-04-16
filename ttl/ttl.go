4d9d68c69fa08b50ddfafc3395fcf8a58bb3fa9f
Marco Peereboom <marco@peereboom.us>
2024-04-16 21:32:26 +0100

    Add ttl package.
    The ttl package can be used to conveniently timeout events. Essentially
    it is a map that has an expiration per key.

---
+++ ttl/ttl.go
@@ -0,0 +1,111 @@
+package ttl
+
+import (
+	"context"
+	"errors"
+	"sync"
+	"time"
+)
+
+var ErrNotFound = errors.New("not found")
+
+type value struct {
+	value any
+
+	expired func(any, any) // called when expired
+	remove  func(any, any) // called when removed
+
+	// Value context
+	ctx    context.Context
+	cancel context.CancelFunc
+}
+
+type TTL struct {
+	mtx sync.Mutex
+
+	m map[any]value
+}
+
+func New(capacity int) (*TTL, error) {
+	return &TTL{
+		m: make(map[any]value, capacity),
+	}, nil
+}
+
+func (tm *TTL) ttl(ctx context.Context, key any) {
+	select {
+	case <-ctx.Done():
+		switch ctx.Err() {
+		case nil:
+			// not yet closed
+
+		case context.DeadlineExceeded:
+			// expired
+			tm.mtx.Lock()
+			defer tm.mtx.Unlock()
+			v, ok := tm.m[key]
+			if !ok {
+				return
+			}
+			if v.expired != nil {
+				go v.expired(key, v.value)
+			}
+
+			// For now assume we aways want to remove key
+			delete(tm.m, key)
+
+		case context.Canceled:
+			// This is the caller calling cancel
+			tm.mtx.Lock()
+			defer tm.mtx.Unlock()
+			v, ok := tm.m[key]
+			if !ok {
+				return
+			}
+			if v.remove != nil {
+				go v.remove(key, v.value)
+			}
+
+			// For now assume we aways want to remove key
+			delete(tm.m, key)
+		}
+	}
+}
+
+func (tm *TTL) Put(pctx context.Context, ttl time.Duration, key any, val any, expired func(any, any), remove func(any, any)) {
+	tm.mtx.Lock()
+	defer tm.mtx.Unlock()
+
+	v := value{
+		value:   val,
+		expired: expired,
+		remove:  remove,
+	}
+	v.ctx, v.cancel = context.WithTimeout(pctx, ttl)
+	tm.m[key] = v
+	go tm.ttl(v.ctx, key)
+}
+
+func (tm *TTL) Get(key any) (any, error) {
+	tm.mtx.Lock()
+	defer tm.mtx.Unlock()
+
+	v, ok := tm.m[key]
+	if !ok {
+		return nil, ErrNotFound
+	}
+	return v.value, nil
+}
+
+func (tm *TTL) Cancel(key any) error {
+	tm.mtx.Lock()
+	defer tm.mtx.Unlock()
+
+	v, ok := tm.m[key]
+	if !ok {
+		return ErrNotFound
+	}
+	v.cancel()
+
+	return nil
+}
---
+++ ttl/ttl_test.go
@@ -0,0 +1,98 @@
+package ttl
+
+import (
+	"context"
+	"fmt"
+	"strconv"
+	"sync"
+	"testing"
+	"time"
+
+	"github.com/davecgh/go-spew/spew"
+)
+
+var (
+	tt *testing.T // ugly but serves a purpose
+	wg sync.WaitGroup
+)
+
+func callback(key any, value any) {
+	v, ok := value.(*sync.WaitGroup)
+	if !ok {
+		panic(fmt.Sprintf("invalid value type: %T", value))
+	}
+	tt.Logf("%v", spew.Sdump(key))
+	v.Done()
+}
+
+func callbackPanic(key any, value any) {
+	panic(fmt.Sprintf("unexpected callback: %v", spew.Sdump(key)))
+}
+
+func TestTTLExpire(t *testing.T) {
+	tt = t
+
+	count := 10
+	tm, err := New(count)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for i := 0; i < count; i++ {
+		wg.Add(1)
+		tm.Put(ctx, time.Second, strconv.Itoa(i), &wg, callback, nil)
+	}
+	for i := 0; i < count; i++ {
+		key := strconv.Itoa(i)
+		_, err := tm.Get(key)
+		if err != nil {
+			t.Fatalf("%v: %v", key, err)
+		}
+	}
+
+	t.Logf("waiting for timeouts")
+	wg.Wait()
+
+	if len(tm.m) != 0 {
+		t.Fatalf("map not empty: %v", len(tm.m))
+	}
+}
+
+func TestTTLCancel(t *testing.T) {
+	tt = t
+
+	count := 10
+	tm, err := New(count)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	for i := 0; i < count; i++ {
+		wg.Add(1)
+		tm.Put(ctx, time.Second, strconv.Itoa(i), &wg, callbackPanic, callback)
+	}
+	for i := 0; i < count; i++ {
+		key := strconv.Itoa(i)
+		_, err := tm.Get(key)
+		if err != nil {
+			t.Fatalf("%v: %v", key, err)
+		}
+		err = tm.Cancel(key)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}
+
+	t.Logf("waiting for cancels")
+	wg.Wait()
+
+	if len(tm.m) != 0 {
+		t.Fatalf("map not empty: %v", len(tm.m))
+	}
+}
