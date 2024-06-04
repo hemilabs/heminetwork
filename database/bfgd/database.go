4e1914cc6c3d40f501f4c55338ca8327b4a6e6e3
ClaytonNorthey92 <clayton.northey@gmail.com>
2024-06-04 13:35:30 -0400

    Stopgap to re-fetch blocks with no children (#131)
    * stopgap for handling blocks with no children in BFG
    
    in BFG, we only increment block height to get each block in the chain; if there is a fork, we don't handle for it.  this introduces a check for blocks with no children, it will then "re-process" the block at that height from electrumx
    
    * index for better query
    
    * Update database/bfgd/postgres/postgres.go
    
    Co-authored-by: Joshua Sing <joshua@bloq.com>
    
    * Update database/bfgd/postgres/postgres.go
    
    Co-authored-by: Joshua Sing <joshua@bloq.com>
    
    * Update database/bfgd/postgres/postgres.go
    
    Co-authored-by: Joshua Sing <joshua@bloq.com>
    
    * removed extra newline
    
    * Update database/bfgd/database_ext_test.go
    
    Co-authored-by: Joshua Sing <joshua@bloq.com>
    
    * handle for collision
    
    * fix variable used
    
    * correct function
    
    * remove collision handling
    
    ---------
    
    Co-authored-by: Joshua Sing <joshua@bloq.com>

--- database/bfgd/database.go
+++ database/bfgd/database.go
@@ -26,6 +26,7 @@ type Database interface {
 	BtcBlockInsert(ctx context.Context, bb *BtcBlock) error
 	BtcBlockByHash(ctx context.Context, hash [32]byte) (*BtcBlock, error)
 	BtcBlockHeightByHash(ctx context.Context, hash [32]byte) (uint64, error)
+	BtcBlocksHeightsWithNoChildren(ctx context.Context) ([]uint64, error)
 
 	// Pop data
 	PopBasisByL2KeystoneAbrevHash(ctx context.Context, aHash [32]byte, excludeUnconfirmed bool) ([]PopBasis, error)
