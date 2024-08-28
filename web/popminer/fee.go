e3e154531791b3da3edad1007ebbda91cb82720e
Joshua Sing <joshua@bloq.com>
2024-08-29 03:32:02 +1000

    popm/wasm: add copyright header and build tags to fee.go

--- web/popminer/fee.go
+++ web/popminer/fee.go
@@ -1,3 +1,9 @@
+// Copyright (c) 2024 Hemi Labs, Inc.
+// Use of this source code is governed by the MIT License,
+// which can be found in the LICENSE file.
+
+//go:build js && wasm
+
 package main
 
 import (
