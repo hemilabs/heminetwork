2a07a917e9df06c9869a45696f89ce4974d67ceb
AL-CT <antoniomartins@protonmail.com>
2025-01-08 23:51:34 +0000

    Removed unecessary check

--- database/tbcd/level/headercache_test.go
+++ database/tbcd/level/headercache_test.go
@@ -17,21 +17,16 @@ func TestHC(t *testing.T) {
 	l := lowIQMapNew(size)
 
 	hs := intHash(0)
-	for k := range 2 {
+	for range 2 {
 		l.Put(&tbcd.BlockHeader{
-			Hash:   hs,
-			Height: uint64(k), // using height to differentiate between headers with same hash
+			Hash: hs,
 		})
 	}
 	if len(l.m) > 1 {
 		t.Fatalf("duplicate headers not excluded by hash")
 	}
 
-	if bh, ok := l.Get(&hs); ok {
-		if bh.Height != 0 {
-			t.Fatalf("existing header overwritten by same hash")
-		}
-	} else {
+	if _, ok := l.Get(&hs); !ok {
 		t.Fatalf("failed to retrieve header present in map")
 	}
 
