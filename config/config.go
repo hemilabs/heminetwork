af290dada90ea334b023e63e08c0474de08753c0
Joshua Sing <joshua@hemi.xyz>
2025-01-29 23:26:03 +1100

    all: update copyright years

--- config/config.go
+++ config/config.go
@@ -1,4 +1,4 @@
-// Copyright (c) 2024 Hemi Labs, Inc.
+// Copyright (c) 2024-2025 Hemi Labs, Inc.
 // Use of this source code is governed by the MIT License,
 // which can be found in the LICENSE file.
 
