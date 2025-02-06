5470e40b102edeb9a94192b6cc8fa4853bd458e8
Marco Peereboom <marco@peereboom.us>
2025-02-06 14:46:28 +0000

    Add example code for bip39 mnemonic/seed stuff and bip32 derivation path

--- bitcoin/bitcoin_test.go
+++ bitcoin/bitcoin_test.go
@@ -5,8 +5,14 @@
 package bitcoin
 
 import (
+	"bytes"
 	"errors"
 	"testing"
+
+	"github.com/btcsuite/btcd/btcutil/hdkeychain"
+	"github.com/btcsuite/btcd/chaincfg"
+	"github.com/davecgh/go-spew/spew"
+	"github.com/tyler-smith/go-bip39"
 )
 
 func TestValidateMerklePathOne(t *testing.T) {
@@ -253,5 +259,101 @@ func TestCheckMerkleChain(t *testing.T) {
 		if err := CheckMerkleChain(test.leaf, test.index, test.merkleHashes, test.merkleRoot); err != nil {
 			t.Errorf("Test %d - failed to validate merkle chain: %v", i, err)
 		}
+	}
+}
+
+func TestEntropyMnemonicSeed(t *testing.T) {
+	// Do the whole rigamarole of Entropy -> Mnemonic -> Seed
+	entropy, err := bip39.NewEntropy(128) // 128 bits of entropy
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(entropy))
+
+	mnemonic, err := bip39.NewMnemonic(entropy)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(mnemonic))
+
+	entropyX, err := bip39.EntropyFromMnemonic(mnemonic)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(entropyX))
+
+	if !bytes.Equal(entropy, entropyX) {
+		t.Fatal("entropy not equal")
+	}
+
+	// The following is educational only on how to get to a master key.
+	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(seed))
+
+	seed2 := bip39.NewSeed(mnemonic, "")
+	t.Log(spew.Sdump(seed2))
+	if !bytes.Equal(seed, seed2) {
+		t.Fatal("seeds not equal")
+	}
+
+	mk, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(mk)) // check on https://iancoleman.io/bip39
+}
+
+func TestDeriveAddresses(t *testing.T) {
+	mnemonic := "dinosaur banner version pistol need area dream champion kiss thank business shrug explain intact puzzle"
+
+	seed, err := bip39.NewSeedWithErrorChecking(mnemonic, "")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	mk, err := hdkeychain.NewMaster(seed, &chaincfg.MainNetParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(mk)) // check on https://iancoleman.io/bip39
+
+	// Derive extended key for hardened account 0: m/0'
+	acct0, err := mk.Derive(hdkeychain.HardenedKeyStart + 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(acct0))
+
+	// Derive extended key for external hardned account 0 m/0'/0'
+	acct0eh, err := acct0.Derive(hdkeychain.HardenedKeyStart + 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(acct0eh))
+	pkHHash, err := acct0eh.Address(&chaincfg.MainNetParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(pkHHash))
+	if pkHHash.String() != "14fjqmZJU7qrRtWHgVjh6jcoKjqu6y1gyM" {
+		t.Fatal("invalid hardned address")
+	}
+
+	// Derive extended key for external account 0 m/0'/0
+	acct0e, err := acct0.Derive(0)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(acct0e))
+	pkHash, err := acct0e.Address(&chaincfg.MainNetParams)
+	if err != nil {
+		t.Fatal(err)
+	}
+	t.Log(spew.Sdump(pkHash))
+	if pkHash.String() != "1KWtnYZmQwnwCdGNwjbzpvh3YibNV6KWb9" {
+		t.Fatal("invalid address")
 	}
 }
