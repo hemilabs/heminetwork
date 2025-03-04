1da125173e384a9879346fea32360f59538e74cf
AL-CT <antoniomartins@protonmail.com>
2025-03-04 11:33:00 +0000

    linter fixes

--- database/tbcd/level/level_test.go
+++ database/tbcd/level/level_test.go
@@ -256,7 +256,7 @@ func TestHeightHashIndexing(t *testing.T) {
 
 	// check each keystone individually
 	for hash, ks := range kssMap {
-		ksr, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, hash)
+		ksr, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, &hash)
 		if err != nil {
 			t.Fatalf("keystone not in db: %v", err)
 		}
@@ -475,7 +475,7 @@ func TestKeystoneUpdate(t *testing.T) {
 			}
 
 			for v, ks := range tti.expectedInDB {
-				_, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, v)
+				_, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, &v)
 				if err != nil {
 					t.Fatalf("keystone not in db: %v", err)
 				}
@@ -495,7 +495,7 @@ func TestKeystoneUpdate(t *testing.T) {
 			}
 
 			for v, ks := range tti.expectedOutDB {
-				_, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, v)
+				_, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, &v)
 				if err == nil {
 					t.Fatalf("keystone in db: %v", spew.Sdump(ks))
 				} else {
@@ -574,14 +574,14 @@ func TestKeystoneDBWindUnwind(t *testing.T) {
 	}
 
 	// Get keystones back out
-	ks1, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, *k1hash)
+	ks1, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, k1hash)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if !reflect.DeepEqual(k1, *ks1) {
 		t.Fatalf("%v%v", spew.Sdump(k1), spew.Sdump(*ks1))
 	}
-	ks2, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, *k2hash)
+	ks2, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, k2hash)
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -595,11 +595,11 @@ func TestKeystoneDBWindUnwind(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	_, err = db.BlockKeystoneByL2KeystoneAbrevHash(ctx, *k1hash)
+	_, err = db.BlockKeystoneByL2KeystoneAbrevHash(ctx, k1hash)
 	if err == nil {
 		t.Fatal("k1 found")
 	}
-	_, err = db.BlockKeystoneByL2KeystoneAbrevHash(ctx, *k2hash)
+	_, err = db.BlockKeystoneByL2KeystoneAbrevHash(ctx, k2hash)
 	if err == nil {
 		t.Fatal("k2 found")
 	}
@@ -648,7 +648,7 @@ func TestKeystoneDBCache(t *testing.T) {
 		}
 
 		for ksHash, ks := range ksm {
-			dks, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, ksHash)
+			dks, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, &ksHash)
 			if err != nil {
 				t.Fatal(err)
 			}
@@ -663,7 +663,7 @@ func TestKeystoneDBCache(t *testing.T) {
 		}
 
 		for ksHash := range ksm {
-			ks, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, ksHash)
+			ks, err := db.BlockKeystoneByL2KeystoneAbrevHash(ctx, &ksHash)
 			if err == nil {
 				t.Fatalf("(cycle %v) deleted keystone found %v", i, spew.Sdump(ks))
 			}
