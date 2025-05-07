23cfaf6e72d42aaaa1e4546463143b15478b3c1c
Marco Peereboom <marco@peereboom.us>
2025-05-08 00:34:08 +0100

    first pass at testing new cache and flseh out cache

--- service/popm/popm_test.go
+++ service/popm/popm_test.go
@@ -11,6 +11,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/davecgh/go-spew/spew"
 	"github.com/juju/loggo"
 
 	"github.com/hemilabs/heminetwork/service/testutil"
@@ -91,6 +92,75 @@ func TestPopMiner(t *testing.T) {
 		if finished {
 			t.Log("Received all expected messages")
 			return
+		}
+	}
+}
+
+func TestTickingPopMiner(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), 7*time.Second)
+	defer cancel()
+
+	_, kssList := testutil.MakeSharedKeystones(40)
+	btcTip := uint(kssList[len(kssList)-1].L1BlockNumber)
+
+	// Create opgeth test server with the request handler.
+	opMsg, opErr, opgeth := testutil.NewMockOpGeth(ctx, kssList)
+	defer opgeth.Close()
+
+	// Create tbc test server with the request handler.
+	tbcMsg, tbcErr, mtbc := testutil.NewMockTBC(ctx, nil, btcTip)
+	defer mtbc.Close()
+
+	// Setup pop miner
+	cfg := NewDefaultConfig()
+	cfg.BitcoinSource = "tbc"
+	cfg.BitcoinURL = "ws" + strings.TrimPrefix(mtbc.URL, "http")
+	cfg.OpgethURL = "ws" + strings.TrimPrefix(opgeth.URL, "http")
+	cfg.BitcoinSecret = "5e2deaa9f1bb2bcef294cc36513c591c5594d6b671fe83a104aa2708bc634c"
+	cfg.LogLevel = "popm=TRACE"
+
+	if err := loggo.ConfigureLoggers(cfg.LogLevel); err != nil {
+		t.Fatal(err)
+	}
+
+	// Create pop miner
+	s, err := NewServer(cfg)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Start pop miner
+	go func() {
+		if err := s.Run(ctx); !errors.Is(err, context.Canceled) {
+			panic(err)
+		}
+	}()
+
+	// messages we expect to receive
+	//expectedMsg := map[string]int{
+	//	"kss_subscribe":          1,
+	//	"kss_getLatestKeystones": 1,
+	//	// tbcapi.CmdBlockKeystoneByL2KeystoneAbrevHashRequest: 2,
+	//	// tbcapi.CmdUTXOsByAddressRequest:  keystoneRequestCount,
+	//	// tbcapi.CmdFeeEstimateRequest:     keystoneRequestCount,
+	//	// tbcapi.CmdTxBroadcastRequest:     keystoneRequestCount,
+	//	// tbcapi.CmdBlockHeaderBestRequest: keystoneRequestCount,
+	//}
+
+	// receive messages and errors from opgeth and tbc
+	for {
+		select {
+		case <-ctx.Done():
+			t.Fatal(ctx.Err())
+		case err = <-opErr:
+			t.Fatal(err)
+		case err = <-tbcErr:
+			t.Fatal(err)
+
+		case n := <-opMsg:
+			log.Infof("opMsg: %v", spew.Sdump(n))
+		case n := <-tbcMsg:
+			log.Infof("tbcMsg: %v", spew.Sdump(n))
 		}
 	}
 }
