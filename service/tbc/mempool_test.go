c4b3092dc697bca327cf5520e47be9961794d706
AL-CT <antoniomartins@protonmail.com>
2025-05-14 19:37:06 +0100

    extra reaping test

--- service/tbc/mempool_test.go
+++ service/tbc/mempool_test.go
@@ -54,7 +54,7 @@ func TestMempoolFees(t *testing.T) {
 	t.Log(spew.Sdump(recs))
 }
 
-func TestMempoolReaping(t *testing.T) {
+func TestMempoolMassReaping(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
 	defer cancel()
 
@@ -104,4 +104,97 @@ func TestMempoolReaping(t *testing.T) {
 		}
 		t.Logf("%v txs reaped at %v, %v txs left", txNum, time.Since(expire).Seconds(), size)
 	}
+}
+
+func TestMempoolReaping(t *testing.T) {
+	type testTableItem struct {
+		name           string
+		txNum          int
+		waitTime       time.Duration
+		txExpireOffset time.Duration
+		expectedOut    int
+	}
+
+	testTable := []testTableItem{
+		{
+			name:        "TestAllOut",
+			txNum:       10,
+			waitTime:    0,
+			expectedOut: 10,
+		},
+		{
+			name:           "TestAllIn",
+			txNum:          10,
+			waitTime:       0,
+			txExpireOffset: time.Hour,
+			expectedOut:    0,
+		},
+		{
+			name:           "TestHalfIn",
+			txNum:          10,
+			waitTime:       500 * time.Millisecond,
+			txExpireOffset: 100 * time.Millisecond,
+			expectedOut:    5,
+		},
+	}
+
+	for _, tti := range testTable {
+		t.Run(tti.name, func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+			defer cancel()
+
+			mp, err := mempoolNew()
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			curTime := time.Now()
+			sleepUntil := curTime.Add(tti.waitTime)
+
+			txsToInsert := createTxs(tti.txNum, curTime, tti.txExpireOffset)
+			for _, tx := range txsToInsert {
+				mp.txsInsert(ctx, &tx)
+			}
+
+			size, _ := mp.stats(ctx)
+			if size != tti.txNum {
+				t.Fatalf("mempool has %v txs, expected %v", size, tti.txNum)
+			}
+
+			time.Sleep(time.Until(sleepUntil))
+			mp.reap()
+
+			for txIndex := range tti.expectedOut {
+				if _, ok := mp.txs[txsToInsert[txIndex].id]; ok {
+					t.Fatalf("expected %v to be out", txsToInsert[txIndex].id)
+				}
+			}
+
+			for i := tti.expectedOut; i != tti.txNum; i++ {
+				if _, ok := mp.txs[txsToInsert[i].id]; !ok {
+					t.Fatalf("expected %v to be in", txsToInsert[i].id)
+				}
+			}
+		})
+	}
+}
+
+func createTxs(count int, expiration time.Time, increase time.Duration) []mempoolTx {
+	txs := make([]mempoolTx, 0, count)
+	for i := range count {
+		uniqueBytes := make([]byte, 32)
+		binary.BigEndian.PutUint32(uniqueBytes, uint32(i))
+		ch, err := chainhash.NewHash(uniqueBytes)
+		if err != nil {
+			panic(err)
+		}
+
+		mptx := mempoolTx{
+			id:      *ch,
+			expires: expiration.Add(time.Duration(i+1) * increase),
+		}
+		txs = append(txs, mptx)
+	}
+
+	return txs
 }
