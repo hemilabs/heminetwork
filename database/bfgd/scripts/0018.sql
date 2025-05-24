b0c64ca4a515574b4c5ffa16747a7d3dce2633e7
ClaytonNorthey92 <clayton.northey@gmail.com>
2025-05-24 11:04:02 -0400

    keep data needed for btc finality all in one table
    prior, we were calculating finality at request/query-time.  this is a relatively-expensive (and complex) calculation. now, store the "lowest btc block height" in the l2_keystones table.  this allows us to return finality with no joins at request/query-time.  this is all done during bitcoin block processing.
    
    our finality lookup for a keystone is, for lack of a better term, now "constant-time" lookup. as the l2_keystone_abrev_hash is indexed (and that's our lookup key).

---
+++ database/bfgd/scripts/0018.sql
@@ -0,0 +1,14 @@
+-- Copyright (c) 2025 Hemi Labs, Inc.
+-- Use of this source code is governed by the MIT License,
+-- which can be found in the LICENSE file.
+
+BEGIN;
+
+UPDATE version SET version = 18;
+
+-- height is unique in BFG as of now; we use electrs as our "source-of-truth"
+-- for the canonical chain
+ALTER TABLE l2_keystones ADD COLUMN lowest_btc_block_height INT NOT NULL DEFAULT 0;
+
+COMMIT;
+
