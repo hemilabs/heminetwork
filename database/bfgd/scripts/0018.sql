d266dff7c665c93e0173735750814f0dd9567a3d
ClaytonNorthey92 <clayton.northey@gmail.com>
2025-05-27 10:08:06 -0400

    remove confusing comment

--- database/bfgd/scripts/0018.sql
+++ database/bfgd/scripts/0018.sql
@@ -6,8 +6,6 @@ BEGIN;
 
 UPDATE version SET version = 18;
 
--- height is unique in BFG as of now; we use electrs as our "source-of-truth"
--- for the canonical chain
 ALTER TABLE l2_keystones ADD COLUMN lowest_btc_block_effective_height INT NOT NULL DEFAULT 0;
 ALTER TABLE l2_keystones ADD COLUMN lowest_btc_block_hash BYTEA NULL;
 
