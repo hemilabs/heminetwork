ee02faa793887b2ddf10e17e48b28617a4536456
AL-CT <antoniomartins@protonmail.com>
2025-05-29 14:44:10 +0100

    revert test changes and unprivate keystoneTxs method

--- service/bfg/bfg_test.go
+++ service/bfg/bfg_test.go
@@ -119,8 +119,7 @@ func TestBFG(t *testing.T) {
 }
 
 func TestFullMockIntegration(t *testing.T) {
-	t.Skip("fix me")
-	ctx, cancel := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	defer cancel()
 
 	errCh := make(chan error, 10)
