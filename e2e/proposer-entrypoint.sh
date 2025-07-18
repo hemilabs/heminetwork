cb4f5694c9b3308b2e0fc5e6d051226e3337e512
Clayton Northey <clayton.northey@gmail.com>
2025-07-18 11:15:49 +0000

    fix proposer's op-node endpoint port

--- e2e/proposer-entrypoint.sh
+++ e2e/proposer-entrypoint.sh
@@ -6,7 +6,7 @@ op-proposer/bin/op-proposer \
     --game-factory-address=$(jq -r '.opChainDeployments[0].disputeGameFactoryProxyAddress' /shared-dir/state.json) \
     --private-key=${ADMIN_PRIVATE_KEY} \
     --l1-eth-rpc=http://geth-l1:8545 \
-    --rollup-rpc=http://op-node:8547 \
+    --rollup-rpc=http://op-node:8548 \
     --resubmission-timeout=15s \
     --safe-abort-nonce-too-low-count=1 \
     --proposal-interval=10s \
