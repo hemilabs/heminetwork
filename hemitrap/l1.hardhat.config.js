d6267a79200d4420b8b00613b17d1b2748f1a5f2
Clayton Northey <clayton.northey@gmail.com>
2025-08-12 18:23:13 +0000

    squashme

--- hemitrap/l1.hardhat.config.js
+++ hemitrap/l1.hardhat.config.js
@@ -1,7 +1,11 @@
 module.exports = {
     networks: {
         hardhat: {
-            chainId: 11155111
+            chainId: 11155111,
+            mining: {
+                auto: false,
+                interval: 24000
+            }
         }
     }
 }
