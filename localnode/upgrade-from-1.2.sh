60d5214607383844e4e2d2c29da3e1b30b6b04a0
Clayton Northey <clayton.northey@gmail.com>
2025-08-19 21:36:00 +0000

    maybe fixed owner of proxyadmin as prank

--- localnode/upgrade-from-1.2.sh
+++ localnode/upgrade-from-1.2.sh
@@ -3,43 +3,15 @@
 
 set -ex
 
-IMPL_SALT=testsalt
-
-DEPLOYMENT_CONTEXT=hemi-testnet 
-
 PRIVATE_KEY=0x2a871d0798f97d79848a013d4936a73bf4cc922c825d33c1cf7073dff6d409c6
 
 RPC_URL='http://172.17.0.1:9988'
-
-
-# upgrade to 1.3
-
-# git fetch origin
-
-# git checkout -f op-contracts/v1.3.0-rc.1
-
-# git submodule update --init --recursive
-
-# IMPL_SALT=$IMPL_SALT DEPLOYMENT_CONTEXT=$DEPLOYMENT_CONTEXT forge script scripts/Deploy.s.sol:Deploy --non-interactive --private-key=$PRIVATE_KEY  --rpc-url $RPC_URL --broadcast
-
-# upgrade to 1.8 / holocene
 
 git fetch op
 
 git checkout -f op-contracts/v1.8.0-rc.4
 
 git submodule update --init --recursive
-
-# cp ~/other/heminetwork/localnode/hemi-testnet.env ./scripts/upgrades/holocene/.env
-
-# (cd ./scripts/upgrades/holocene ; just build-image)
-
-# (cd ./scripts/upgrades/holocene ; just run ~/other/heminetwork/localnode/hemi-testnet-deploy-config.json)
-
-# # Clayton note: pin
-# git checkout origin/hemi
-
-# git submodule update --init --recursive
 
 (cd ./../../op-deployer ; just build)
 
@@ -54,7 +26,7 @@ tail -n 15 ./opcm-output.json
 
 cat >tmp-config.json <<EOL
 {
-  "prank": "0xc43ED1E8D70d0e5801514833fAD3D93Ba16Da4Aa",
+  "prank": "0x382D0AA958998408DD7695c8965C46BdaBBC3003",
   "opcm": "$(tail -n 15 ./opcm-output.json | jq -r '.OpcmProxy')",
   "chainConfigs": [
     {
