7292d542b00dd6799498d02a6744ebcc1edd8834
Marco Peereboom <marco@peereboom.us>
2025-08-27 13:39:41 +0100

    Add basic replicator test

--- database/gkvdb/replicator_test.go
+++ database/gkvdb/replicator_test.go
@@ -2,10 +2,13 @@ package gkvdb
 
 import (
 	"bytes"
+	"context"
 	"io"
+	"path/filepath"
 	"reflect"
 	"strconv"
 	"testing"
+	"time"
 
 	"github.com/kelindar/binary"
 )
@@ -72,6 +75,95 @@ func TestJournalStream(t *testing.T) {
 		}
 		if !reflect.DeepEqual(jopExected, jop) {
 			t.Fatal("not equal")
+		}
+	}
+}
+
+func TestReplicateDirect(t *testing.T) {
+	// First create source and destination
+	home := t.TempDir()
+	tables := []string{"table1", "table2"}
+	homeSource := filepath.Join(home, "source")
+	dbSource, err := NewLevelDB(DefaultLevelConfig(homeSource, tables))
+	if err != nil {
+		t.Fatal(err)
+	}
+	homeDestination := filepath.Join(home, "destination")
+	dbDestination, err := NewLevelDB(DefaultLevelConfig(homeDestination, tables))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create replicator database
+	homeJournal := filepath.Join(home, "journal")
+	rcfg := DefaultReplicatorConfig(homeJournal, Direct)
+	db, err := NewReplicatorDB(rcfg, dbSource, dbDestination)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	ctx, cancel := context.WithTimeout(t.Context(), 15*time.Second)
+	defer cancel()
+
+	err = db.Open(ctx)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Individual Puts
+	maxPuts := 1337
+	valueOffset := 10000
+	recordsPerTable := make([]int, len(tables))
+	for i := 0; i < maxPuts; i++ {
+		tx := i % len(tables)
+		table := tables[tx]
+		// t.Logf("%v %v", table, i)
+		err := db.Put(ctx, table, []byte(strconv.Itoa(i)),
+			[]byte(strconv.Itoa(i+valueOffset)))
+		if err != nil {
+			t.Fatal(err)
+		}
+		recordsPerTable[tx]++
+	}
+
+	// Verify that we have them in the replicator db (really the source)
+	// and in the destination db.
+	for i := 0; i < maxPuts; i++ {
+		tx := i % len(tables)
+		table := tables[tx]
+		// t.Logf("%v %v", table, i)
+		value, err := db.Get(ctx, table, []byte(strconv.Itoa(i)))
+		if err != nil {
+			t.Fatal(err)
+		}
+		expectedValue := []byte(strconv.Itoa(i + valueOffset))
+		if !bytes.Equal(expectedValue, value) {
+			t.Fatal("not equal")
+		}
+
+		// Now fish it out of destination.
+		dValue, err := dbDestination.Get(ctx, table, []byte(strconv.Itoa(i)))
+		if err != nil {
+			t.Fatal(err)
+		}
+		if !bytes.Equal(expectedValue, dValue) {
+			t.Fatal("not equal")
+		}
+	}
+
+	// Iterate over all tables and count records to compare to source
+	for k := range tables {
+		it, err := dbDestination.NewIterator(ctx, tables[k])
+		if err != nil {
+			t.Fatal(err)
+		}
+		x := 0
+		for it.Next(ctx) {
+			x++
+		}
+		if x != recordsPerTable[k] {
+			t.Fatalf("%v: got %v wanted %v",
+				tables[k], x, recordsPerTable[k])
 		}
 	}
 }
