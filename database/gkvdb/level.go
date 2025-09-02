d844221ecf71263300b2fa132546f90c32c07987
AL-CT <antoniomartins@protonmail.com>
2025-09-03 00:57:27 +0100

    add ErrDBOpen to level

--- database/gkvdb/level.go
+++ database/gkvdb/level.go
@@ -64,6 +64,10 @@ func NewLevelDB(cfg *LevelConfig) (Database, error) {
 }
 
 func (b *levelDB) Open(_ context.Context) error {
+	if b.db != nil {
+		return ErrDBOpen
+	}
+
 	ldb, err := leveldb.OpenFile(b.cfg.Home, &opt.Options{
 		BlockCacheEvictRemoved: true,
 		Compression:            opt.NoCompression,
