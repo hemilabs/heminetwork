0f31aeace0ada1a566b209ee47ad0e912004abfe
AL-CT <antoniomartins@protonmail.com>
2025-09-18 22:17:15 +0100

    change e2e tests to larry

--- e2e/e2e_ext_test.go
+++ e2e/e2e_ext_test.go
@@ -295,7 +295,7 @@ func TestGetFinalitiesByL2KeystoneBFGInheritingfinality(t *testing.T) {
 		},
 	})
 
-	if err := db.Close(); err != nil {
+	if err := db.Close(ctx); err != nil {
 		t.Fatal(err)
 	}
 
@@ -407,7 +407,7 @@ func TestGetFinalitiesByL2KeystoneBFGInOrder(t *testing.T) {
 		},
 	})
 
-	if err := db.Close(); err != nil {
+	if err := db.Close(ctx); err != nil {
 		t.Fatal(err)
 	}
 
@@ -509,7 +509,7 @@ func TestGetFinalitiesByL2KeystoneBFGNotFoundOnChain(t *testing.T) {
 
 	createChainWithKeystones(ctx, t, db, 13, map[uint64]tbcd.Keystone{})
 
-	if err := db.Close(); err != nil {
+	if err := db.Close(ctx); err != nil {
 		t.Fatal(err)
 	}
 
@@ -633,7 +633,7 @@ func TestGetFinalitiesByL2KeystoneBFGNotFoundOpGeth(t *testing.T) {
 		},
 	})
 
-	if err := db.Close(); err != nil {
+	if err := db.Close(ctx); err != nil {
 		t.Fatal(err)
 	}
 
