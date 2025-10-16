753ac371b535a1e469d5595a4a34625d677d6dc6
Marco Peereboom <marco@peereboom.us>
2025-10-16 17:46:33 +0100

    share with max

--- go.mod
+++ go.mod
@@ -32,7 +32,9 @@ require (
 require (
 	dario.cat/mergo v1.0.2 // indirect
 	github.com/Azure/go-ansiterm v0.0.0-20250102033503-faa5f7b0171c // indirect
+	github.com/DataDog/zstd v1.5.2 // indirect
 	github.com/Microsoft/go-winio v0.6.2 // indirect
+	github.com/VictoriaMetrics/fastcache v1.13.0 // indirect
 	github.com/aead/siphash v1.0.1 // indirect
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/bits-and-blooms/bitset v1.22.0 // indirect
@@ -56,9 +58,11 @@ require (
 	github.com/ethereum/c-kzg-4844/v2 v2.1.3 // indirect
 	github.com/ethereum/go-verkle v0.2.2 // indirect
 	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/ferranbt/fastssz v0.1.4 // indirect
 	github.com/go-logr/logr v1.4.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/go-ole/go-ole v1.3.0 // indirect
+	github.com/gofrs/flock v0.12.1 // indirect
 	github.com/gogo/protobuf v1.3.2 // indirect
 	github.com/golang/snappy v1.0.0 // indirect
 	github.com/google/uuid v1.6.0 // indirect
@@ -66,8 +70,12 @@ require (
 	github.com/holiman/uint256 v1.3.2 // indirect
 	github.com/kkdai/bstream v1.0.0 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
+	github.com/klauspost/cpuid/v2 v2.0.9 // indirect
 	github.com/lufia/plan9stats v0.0.0-20250317134145-8bc96cf8fc35 // indirect
 	github.com/magiconair/properties v1.8.10 // indirect
+	github.com/mattn/go-runewidth v0.0.13 // indirect
+	github.com/minio/sha256-simd v1.0.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.1 // indirect
 	github.com/moby/docker-image-spec v1.3.1 // indirect
 	github.com/moby/go-archive v0.1.0 // indirect
 	github.com/moby/patternmatcher v0.6.0 // indirect
@@ -77,6 +85,7 @@ require (
 	github.com/moby/term v0.5.2 // indirect
 	github.com/morikuni/aec v1.0.0 // indirect
 	github.com/munnerz/goautoneg v0.0.0-20191010083416-a7dc8b61c822 // indirect
+	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/opencontainers/go-digest v1.0.0 // indirect
 	github.com/opencontainers/image-spec v1.1.1 // indirect
 	github.com/otiai10/mint v1.6.3 // indirect
@@ -86,6 +95,7 @@ require (
 	github.com/prometheus/client_model v0.6.2 // indirect
 	github.com/prometheus/common v0.66.1 // indirect
 	github.com/prometheus/procfs v0.17.0 // indirect
+	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/shirou/gopsutil v3.21.11+incompatible // indirect
 	github.com/sirupsen/logrus v1.9.3 // indirect
 	github.com/stretchr/objx v0.5.2 // indirect
@@ -107,8 +117,11 @@ require (
 	golang.org/x/sync v0.16.0 // indirect
 	golang.org/x/sys v0.36.0 // indirect
 	golang.org/x/time v0.12.0 // indirect
+	golang.org/x/xerrors v0.0.0-20220517211312-f3a8303e98df // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250728155136-f173205681a0 // indirect
 	google.golang.org/grpc v1.74.2 // indirect
 	google.golang.org/protobuf v1.36.8 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
+
+replace github.com/ethereum/go-ethereum => /home/marco/tmp/go-ethereum
---
+++ shit/shit2.go
@@ -0,0 +1,132 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/ethereum/go-ethereum/core/rawdb"
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/trie"
+	"github.com/ethereum/go-ethereum/trie/trienode"
+	"github.com/ethereum/go-ethereum/triedb"
+	"github.com/ethereum/go-ethereum/triedb/pathdb"
+)
+
+func main() {
+	diskdb, err := rawdb.Open(rawdb.NewMemoryDatabase(), rawdb.OpenOptions{
+		Ancient: "x", // os.TempDir(),
+	})
+	if err != nil {
+		panic(err)
+	}
+
+	d := pathdb.Defaults
+	d.NoAsyncFlush = true
+	tdb := triedb.NewDatabase(diskdb, &triedb.Config{
+		PathDB: d,
+	})
+
+	// V1
+	trV0, err := trie.New(trie.StateTrieID(types.EmptyRootHash), tdb)
+	if err != nil {
+		panic(err)
+	}
+	if err := trV0.Update([]byte("apple"), []byte("red")); err != nil {
+		panic(err)
+	}
+	if err := trV0.Update([]byte("banana"), []byte("yellow")); err != nil {
+		panic(err)
+	}
+	if err := trV0.Update([]byte("cherry"), []byte("dark red")); err != nil {
+		panic(err)
+	}
+	rootV1, nodesV1 := trV0.Commit(true)
+	nodesetV1 := trienode.NewWithNodeSet(nodesV1)
+	stateV1 := triedb.NewStateSet()
+	if err := tdb.Update(rootV1, types.EmptyRootHash, 0, nodesetV1, stateV1); err != nil {
+		panic(err)
+	}
+	fmt.Println("Root V1:", rootV1)
+
+	// V2
+	trV1, err := trie.New(trie.TrieID(rootV1), tdb)
+	if err != nil {
+		panic(err)
+	}
+	if err := trV1.Update([]byte("date"), []byte("brown")); err != nil {
+		panic(err)
+	}
+	rootV2, nodesV2 := trV1.Commit(true)
+	nodesetV2 := trienode.NewWithNodeSet(nodesV2)
+	stateV2 := triedb.NewStateSet()
+	if err := tdb.Update(rootV2, rootV1, 1, nodesetV2, stateV2); err != nil {
+		panic(err)
+	}
+	fmt.Println("Committed Root V2:", rootV2)
+
+	// V3
+	trV2, err := trie.New(trie.TrieID(rootV2), tdb)
+	if err != nil {
+		panic(err)
+	}
+	if err := trV2.Update([]byte("elderberry"), []byte("black")); err != nil {
+		panic(err)
+	}
+	if err := trV2.Update([]byte("fig"), []byte("purple")); err != nil {
+		panic(err)
+	}
+	rootV3, nodesV3 := trV2.Commit(true)
+	nodesetV3 := trienode.NewWithNodeSet(nodesV3)
+	stateV3 := triedb.NewStateSet()
+	if err := tdb.Update(rootV3, rootV2, 2, nodesetV3, stateV3); err != nil {
+		panic(err)
+	}
+	fmt.Println("Committed Root V3:", rootV3)
+
+	// Print
+	trV1, err = trie.New(trie.TrieID(rootV1), tdb)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("\nState at Root V1:")
+	showKeys(trV1, []string{"apple", "banana", "cherry", "date", "elderberry", "fig"})
+
+	trV2, err = trie.New(trie.TrieID(rootV2), tdb)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("\nState at Root V2:")
+	showKeys(trV2, []string{"apple", "banana", "cherry", "date", "elderberry", "fig"})
+
+	trV3, err := trie.New(trie.TrieID(rootV3), tdb)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("\nState at Root V3:")
+	showKeys(trV3, []string{"apple", "banana", "cherry", "date", "elderberry", "fig"})
+
+	// Got to V2
+	//if err := tdb.Enable(types.EmptyRootHash); err != nil {
+	//	panic(err)
+	//}
+	//if err := tdb.Enable(rootV1); err != nil {
+	//	panic(err)
+	//}
+	// rawdb.WriteStateID(diskdb, types.EmptyRootHash, 0)
+	// rawdb.WriteStateID(diskdb, rootV1, 1)
+	// rawdb.WriteStateID(diskdb, rootV2, 2)
+	// rawdb.WriteStateID(diskdb, rootV3, 3)
+	if err := tdb.Recover(rootV3); err != nil {
+		panic(err)
+	}
+}
+
+func showKeys(tr *trie.Trie, keys []string) {
+	for _, k := range keys {
+		val, _ := tr.Get([]byte(k))
+		if val == nil {
+			fmt.Printf("%s: <nil>\n", k)
+		} else {
+			fmt.Printf("%s: %s\n", k, val)
+		}
+	}
+}
