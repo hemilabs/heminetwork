7eed7790671d6db417e43c11c7d9b5243fd8153e
mk0walsk <mateuszkowalskiiiiiq@gmail.com>
2025-11-24 22:36:37 +0800

    Align Go docs and comments

--- hemi/pop/pop.go
+++ hemi/pop/pop.go
@@ -39,7 +39,7 @@ func MinerAddressFromString(address string) (*MinerAddress, error) {
 
 // TransactionL2 rename to Transaction and fixup this code
 type TransactionL2 struct {
-	L2Keystone *hemi.L2KeystoneAbrev // XXX wtf is this a pointer?
+	L2Keystone *hemi.L2KeystoneAbrev // XXX why is this a pointer?
 }
 
 // Serialize serializes a PoP transaction to its byte representation.
