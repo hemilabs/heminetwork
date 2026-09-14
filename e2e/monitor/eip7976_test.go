711f12cae46b27111c153620018dd6ff97b07288
ClaytonNorthey92 <clayton.northey@gmail.com>
2026-09-14 12:51:50 -0400

    debugging

--- e2e/monitor/eip7976_test.go
+++ e2e/monitor/eip7976_test.go
@@ -180,6 +180,7 @@ func (h *harness) send(to *common.Address, data []byte, gasLimit uint64) *types.
 	retries := 10
 
 	for range retries {
+		time.Sleep(1 * time.Second)
 		nonce, err := h.client.PendingNonceAt(h.ctx, h.from)
 		if err != nil {
 			h.t.Logf("fetching nonce: %v", err)
