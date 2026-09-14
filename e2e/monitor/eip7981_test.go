a97401ea628cab0df608f5fd8ab062d0119f60cd
ClaytonNorthey92 <clayton.northey@gmail.com>
2026-09-14 15:21:43 -0400

    7981

---
+++ e2e/monitor/eip7981_test.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"crypto/ecdsa"
+	"math/big"
+	"testing"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/core/types"
+)
+
+var accessList = []types.AccessTuple{
+	types.AccessTuple{
+		Address:     common.HexToAddress("0xed94c848bee8df6bc65456064eee602619b0cea0"),
+		StorageKeys: []common.Hash{},
+	},
+}
+
+func EIP7981_RejectsInsufficientGasLimit(t *testing.T, key *ecdsa.PrivateKey) {
+	h := newHarness(t, key)
+
+	accessListBytes := 0
+	for _, a := range accessList {
+		accessListBytes += len(a.Address)
+		for _, s := range a.StorageKeys {
+			accessListBytes += len(s)
+		}
+	}
+
+	data := nonZeroByteData(10_000)
+	minRequired := txBaseCost + floorCost(data)
+
+	// One gas below the required minimum must be rejected.
+	h.sendExpectingRejectionWithAccessList(&dummyRecipient, data, minRequired-1)
+
+	// Sanity check: the same transaction at exactly the required minimum
+	// must be accepted and mined successfully.
+	receipt := h.send(&dummyRecipient, data, minRequired)
+	if receipt.Status != types.ReceiptStatusSuccessful {
+		t.Fatalf("transaction at exactly the minimum required gas limit (%d) failed (status=%d)",
+			minRequired, receipt.Status)
+	}
+	if receipt.GasUsed != minRequired {
+		t.Fatalf("gasUsed = %d, want %d", receipt.GasUsed, minRequired)
+	}
+}
+
+func (h *harness) sendExpectingRejectionWithAccessList(to *common.Address, data []byte, gasLimit uint64) {
+	h.t.Helper()
+
+	nonce, err := h.client.PendingNonceAt(h.ctx, h.from)
+	if err != nil {
+		h.t.Fatalf("fetching nonce: %v", err)
+	}
+
+	tx := types.NewTx(&types.AccessListTx{
+		Nonce:      nonce,
+		To:         to,
+		Value:      big.NewInt(1),
+		Gas:        gasLimit,
+		GasPrice:   h.gasPrice,
+		Data:       data,
+		AccessList: accessList,
+	})
+	signer := types.LatestSignerForChainID(h.chainID)
+	signedTx, err := types.SignTx(tx, signer, h.key)
+	if err != nil {
+		h.t.Fatalf("signing tx: %v", err)
+	}
+
+	err = h.client.SendTransaction(h.ctx, signedTx)
+	if err == nil {
+		h.t.Logf("expected node to reject tx with gas limit %d below the "+
+			"EIP-7981 floor, but it was accepted (hash %s)", gasLimit, signedTx.Hash())
+	}
+	// Rejected before entering the pool: nonce was not consumed.
+}
--- e2e/monitor/main_test.go
+++ e2e/monitor/main_test.go
@@ -348,6 +348,9 @@ func testL1L2Comms(t *testing.T, l1Endpoint string, l2Endpoint string, l2NonSequ
 			// check for the existence of EIP-7976, do not exhaustively test it
 			EIP7976_RejectsInsufficientGasLimit(t, privateKey)
 
+			// similarly check for the existence of EIP-7981
+			EIP7981_RejectsInsufficientGasLimit(t, privateKey)
+
 			invalidTxidRetries := 10
 			for i := range invalidTxidRetries {
 				if err := sendToTBCPrecompileWithInvalidTXID(t, ctx, l1Client, privateKey); err != nil {
