c7fb51fd47cece3ff48242a8666ab76d529d0a2a
ClaytonNorthey92 <clayton.northey@gmail.com>
2026-09-21 10:55:28 -0400

    remove field

--- e2e/genesisl2.sh
+++ e2e/genesisl2.sh
@@ -78,6 +78,7 @@ echo "$(jq '.l1BlockTime = 3' /shared-dir/deploy-config.json)" > /shared-dir/dep
 echo "$(jq '.proofMaturityDelaySeconds = 10' /shared-dir/deploy-config.json)" > /shared-dir/deploy-config.json
 echo "$(jq '.preimageOracleChallengePeriod = 10' /shared-dir/deploy-config.json)" > /shared-dir/deploy-config.json
 echo "$(jq 'del(.customGasTokenAddress)' /shared-dir/deploy-config.json)" > /shared-dir/deploy-config.json
+echo "$(jq 'del(.requiredProtocolVersion)' /shared-dir/deploy-config.json)" > /shared-dir/deploy-config.json
 echo "$(jq '.operatorFeeVaultRecipient = "0x78697c88847dfbbb40523e42c1f2e28a13a170be"' /shared-dir/deploy-config.json)" > /shared-dir/deploy-config.json
 echo "$(jq '.operatorFeeVaultWithdrawalNetwork = 1' /shared-dir/deploy-config.json)" > /shared-dir/deploy-config.json
 
